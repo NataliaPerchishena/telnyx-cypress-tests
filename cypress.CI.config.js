ab3369f343c4b19dfcabba4a39deca5b77e1bf3b
Nataliya Perchyshena <chilly233@gmail.com>
2025-07-18 12:57:12 +0200

    change config patern, add check viewport in beforeEach

--- cypress.CI.config.js
+++ cypress.CI.config.js
@@ -4,10 +4,7 @@ module.exports = defineConfig({
   projectId: "7rza8s",
 
   e2e: {
-    specPattern: [
-      "cypress/e2e/desktop/**/*.cy.js",
-      "cypress/e2e/mob/**/*.cy.js",
-    ],
+    specPattern: "cypress/e2e/desktop/**/*.cy.js",
     baseUrl: "https://telnyx.com",
     defaultCommandTimeout: 20000,
     viewportWidth: 1280,
@@ -16,10 +13,6 @@ module.exports = defineConfig({
 
     setupNodeEvents(on, config) {
       // implement node event listeners here
-      installLogsPrinter(on, {
-        printLogsToConsole: "always",
-        printLogsToFile: "onFail",
-      });
     },
   },
   video: false,
--- cypress.CI_mob.config.js
+++ cypress.CI_mob.config.js
@@ -13,10 +13,6 @@ module.exports = defineConfig({
     chromeWebSecurity: false,
     setupNodeEvents(on, config) {
       // implement node event listeners here
-      installLogsPrinter(on, {
-        printLogsToConsole: "always",
-        printLogsToFile: "onFail",
-      });
     },
   },
   video: false,
